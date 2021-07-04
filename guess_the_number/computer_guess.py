50eb3487071510749489af67f8591a951e5227a2
francinemadoh@gmail.com <francinemadoh@gmail.com>
2021-07-04 17:54:35 +0100

    fix the case if the number low is equal to high

--- guess_the_number/computer_guess.py
+++ guess_the_number/computer_guess.py
@@ -5,7 +5,10 @@ def computer_guess(number):
     high = number
     result = ''
     while result != 'c':
-        guess = random.randint(low, high)
+        if low != high:
+          guess = random.randint(low, high)
+        else:
+            guess = low
         result = input(f'Is { guess } too high (H) too low (L) or correct (c)? ').lower()
         if(result == 'h'):
             high = guess - 1
@@ -13,7 +16,7 @@ def computer_guess(number):
             low = guess + 1
     print(f'Congratulations the computer did guess your number which is { guess }')
 
-computer_guess(100)
+computer_guess(1000)
 
 
 
